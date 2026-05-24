e8e48daec10ebeec1df0d1b0964901b922c086f7
Badi Labassi <194211+badilabassi@users.noreply.github.com>
2026-05-24 09:14:45 -0400

    Update project configuration and add API support
    - Renamed project from "d4filter" to "diablo4-filter-viewer" in package.json and package-lock.json.
    - Updated development script in package.json to set NODE_ENV.
    - Added Vercel configuration file for deployment.
    - Introduced API endpoint in api/index.ts to handle requests.
    - Added .vercel to .gitignore to exclude Vercel configuration from version control.

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import { router } from '../app/router.ts'
+
+export default {
+  async fetch(request: Request) {
+    try {
+      return await router.fetch(request)
+    } catch (error) {
+      if (!(request.signal.aborted && error === request.signal.reason)) {
+        console.error(error)
+      }
+      return new Response('Internal Server Error', { status: 500 })
+    }
+  },
+}
